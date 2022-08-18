ee68695d91f08fca3601bc731b1c0f5e5a810b1b
Georgii Klimov <klimovgg@gmail.com>
2022-08-18 22:12:51 +0200

    create actions for ToDo

--- src/app/components/to-do/state/to-do.actions.ts
+++ src/app/components/to-do/state/to-do.actions.ts
@@ -1,5 +1,30 @@
-import { createAction } from '@ngrx/store';
+import {Action} from '@ngrx/store';
+import {Task} from "../../task-item/task-item.model";
 
-export const increment = createAction('[Counter Component] Increment');
-export const decrement = createAction('[Counter Component] Decrement');
-export const reset = createAction('[Counter Component] Reset');
+export enum ToDoActionTypes {
+  CREATE_TASK = '[To Do] Create new task',
+  CHANGE_TASK = '[To Do] Edit task',
+  DELETE_TASK = '[To Do] Edit task',
+}
+
+export class ToDoTaskCreate implements Action {
+  readonly type = ToDoActionTypes.CREATE_TASK;
+  constructor(public payload: Task) {}
+}
+
+export class ToDoTaskChange implements Action {
+  readonly type = ToDoActionTypes.CHANGE_TASK;
+  constructor(public payload: string) {}
+}
+
+export class ToDoTaskDelete implements Action {
+  readonly type = ToDoActionTypes.DELETE_TASK;
+  constructor(public payload: number) {}
+}
+
+export type ToDoActionUnion =
+  | ToDoTaskCreate
+  | ToDoTaskChange
+  | ToDoTaskDelete
+
+
